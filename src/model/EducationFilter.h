143aaab958c6f7bfb1683b0c0c8a1e0a955447c2
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-07-30 08:07:27 +0700

    Update EducationFilter.h

--- src/model/EducationFilter.h
+++ src/model/EducationFilter.h
@@ -1,1 +1,18 @@
 
+// model/EducationFilter.h
+#ifndef EDUCATIONFILTER_H
+#define EDUCATIONFILTER_H
+
+#include "CVFilter.h"
+#include <string>
+
+class EducationFilter : public CVFilter {
+private:
+    std::string targetEducation;
+
+public:
+    explicit EducationFilter(std::string edu);
+    bool match(const CV& cv) const override;
+};
+
+#endif // EDUCATIONFILTER_H
