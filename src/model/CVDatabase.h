49d582927345714d48cf89bb94c33d67f58dacd5
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-07-30 08:08:48 +0700

    Update CVDatabase.h

--- src/model/CVDatabase.h
+++ src/model/CVDatabase.h
@@ -1,1 +1,26 @@
 
+// model/CVDatabase.h
+#ifndef CVDATABASE_H
+#define CVDATABASE_H
+
+#include <vector>
+#include "CV.h"
+
+class CVDatabase {
+private:
+    std::vector<CV*> list_cv;
+
+public:
+    CVDatabase() = default;
+    ~CVDatabase();
+
+    void addCV(CV* cv);
+    void removeCV(int id);
+    
+    // Thêm const correctness giữ nguyên phong cách
+    CV* getById(int id) const;
+    std::vector<CV*> getAll() const;
+    void clear();
+};
+
+#endif // CVDATABASE_H
