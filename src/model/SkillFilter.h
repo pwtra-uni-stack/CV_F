36c75ec1c363c383b1fa3fc3fea465bf8252bea7
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-07-30 08:05:06 +0700

    Update SkillFilter.h

--- src/model/SkillFilter.h
+++ src/model/SkillFilter.h
@@ -1,1 +1,19 @@
 
+// model/SkillFilter.h
+#ifndef SKILLFILTER_H
+#define SKILLFILTER_H
+
+#include "CVFilter.h"
+#include <vector>
+#include <string>
+
+class SkillFilter : public CVFilter {
+private:
+    std::vector<std::string> required_skills;
+
+public:
+    explicit SkillFilter(std::vector<std::string> skills);
+    bool match(const CV& cv) const override;
+};
+
+#endif // SKILLFILTER_H
