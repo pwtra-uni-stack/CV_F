a8fd45cbd41a6444433acacf360cbe809ab7596a
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-07-30 07:56:50 +0700

    Update Candidate.cpp

--- src/model/Candidate.cpp
+++ src/model/Candidate.cpp
@@ -1,1 +1,19 @@
 
+// model/Candidate.h
+#ifndef CANDIDATE_H
+#define CANDIDATE_H
+
+#include <string>
+
+class Candidate {
+public:
+    std::string candidate_id;
+    std::string name;
+    std::string email;
+    std::string phone;
+
+    Candidate();
+    Candidate(std::string id, std::string name, std::string email, std::string phone);
+};
+
+#endif // CANDIDATE_H
