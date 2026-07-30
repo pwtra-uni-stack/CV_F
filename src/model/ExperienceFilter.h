8efab2d25f436cb4f50ae8165062e570f383fb56
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-07-30 08:06:03 +0700

    Update ExperienceFilter.h

--- src/model/ExperienceFilter.h
+++ src/model/ExperienceFilter.h
@@ -1,1 +1,17 @@
 
+// model/ExperienceFilter.h
+#ifndef EXPERIENCEFILTER_H
+#define EXPERIENCEFILTER_H
+
+#include "CVFilter.h"
+
+class ExperienceFilter : public CVFilter {
+private:
+    int minYears;
+
+public:
+    explicit ExperienceFilter(int years);
+    bool match(const CV& cv) const override;
+};
+
+#endif // EXPERIENCEFILTER_H
