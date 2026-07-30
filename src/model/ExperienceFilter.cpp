5278255a37c3d5f3056bd3ad815efe4d23238c2f
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-07-30 08:06:47 +0700

    Update ExperienceFilter.cpp

--- src/model/ExperienceFilter.cpp
+++ src/model/ExperienceFilter.cpp
@@ -1,1 +1,9 @@
+// model/ExperienceFilter.cpp
+#include "ExperienceFilter.h"
+
+ExperienceFilter::ExperienceFilter(int years) : minYears(years) {}
+
+bool ExperienceFilter::match(const CV& cv) const {
+    return cv.experienceYears >= minYears;
+}
 
