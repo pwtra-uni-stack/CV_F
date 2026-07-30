497599a2f3c77d15b57b6f1a6a434bccd5ff6ca6
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-07-30 08:08:18 +0700

    Update EducationFilter.cpp

--- src/model/EducationFilter.cpp
+++ src/model/EducationFilter.cpp
@@ -1,1 +1,9 @@
 
+// model/EducationFilter.cpp
+#include "EducationFilter.h"
+
+EducationFilter::EducationFilter(std::string edu) : targetEducation(edu) {}
+
+bool EducationFilter::match(const CV& cv) const {
+    return cv.education.find(targetEducation) != std::string::npos;
+}
