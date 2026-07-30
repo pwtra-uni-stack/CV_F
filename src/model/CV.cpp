3e7cd9742b9764d9c86f2ce2e683d3c366e3efd6
Đỗ Khánh Linh <dokhanhlinh6556@gmail.com>
2026-07-30 08:01:26 +0700

    Update CV.cpp

--- src/model/CV.cpp
+++ src/model/CV.cpp
@@ -1,1 +1,18 @@
 
+// model/CV.cpp
+#include "CV.h"
+
+CV::CV() 
+    : id(""), candidate(nullptr), experienceYears(0), education(""), raw_text(""), score(0) {}
+
+CV::CV(std::string id, Candidate* cand, std::vector<std::string> sk, 
+       int exp, std::string edu, std::string raw, int score)
+    : id(id), candidate(cand), skills(sk), experienceYears(exp), 
+      education(edu), raw_text(raw), score(score) {}
+
+CV::~CV() {
+    if (candidate != nullptr) {
+        delete candidate;
+        candidate = nullptr;
+    }
+}
